4854fbea2167614d65c3fa7de27824f98c551273
sal garcia <salgarcia940@gmail.com>
2021-06-17 23:09:42 -0700

    completed prep javacript for loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var e = 0; e < 20; e += 2) {
+  console.log(e);
+}
+
+for (var o = 100; o > 0; o--) {
+  console.log('Time till explosion');
+}
